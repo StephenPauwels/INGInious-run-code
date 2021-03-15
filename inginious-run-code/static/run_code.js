1f441365dfc20a0f7cc9256cbcd7ea259abfef56
Stephen <stephen.pauwels@uantwerpen.be>
2021-03-15 15:21:36 +0100

    Little changes

--- inginious-run-code/static/run_code.js
+++ inginious-run-code/static/run_code.js
@@ -2,7 +2,7 @@ function load_input_run_code(submissionid, key, input) {
     load_input_code(submissionid, key, input);
 }
 
-function studio_init_template_extra_test(well, pid, problem)
+function studio_init_template_run_code(well, pid, problem)
 {
     studio_init_template_code(well, pid, problem);
 }
