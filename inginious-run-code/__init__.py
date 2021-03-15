97b97465538f94ca3b0fb05986efb3a73abf6e8e
Stephen <stephen.pauwels@uantwerpen.be>
2021-03-15 15:14:14 +0100

    Little changes

--- inginious-run-code/__init__.py
+++ inginious-run-code/__init__.py
@@ -19,7 +19,7 @@ class RunCodeProblem(CodeProblem):
     def get_type_name(cls, language):
         return _("run_code")
 
-class DisplayableRunCodeProblem(RunCodeProblem, DisplayableCodeProblem):
+class DisplayableRunCodeProblem(RunCodeProblem, DisplayableProblem):
     """ A displayable match problem """
 
     def __init__(self, problemid, content, translations, taskfs):
@@ -28,6 +28,25 @@ class DisplayableRunCodeProblem(RunCodeProblem, DisplayableCodeProblem):
     @classmethod
     def get_type_name(cls, language):
         return _("run_code")
+
+    def adapt_input_for_backend(self, input_data):
+        return input_data
+
+    def show_input(self, template_helper, language, seed):
+        """ Show BasicCodeProblem and derivatives """
+        header = ParsableText(self.gettext(language,self._header), "rst",
+                              translation=self.get_translation_obj(language))
+        return template_helper.render("tasks/code.html", inputId=self.get_id(), header=header,
+                                      lines=8, maxChars=0, language=self._language, optional=self._optional,
+                                      default=self._default)
+
+    @classmethod
+    def show_editbox(cls, template_helper, key, language):
+        return template_helper.render("course_admin/subproblems/code.html", key=key, multiline=True)
+
+    @classmethod
+    def show_editbox_templates(cls, template_helper, key, language):
+        return ""
 
 class StaticMockPage(INGIniousPage):
     # TODO: Replace by shared static middleware and let webserver serve the files
